5f188f7b8de70f8e0e8d2c04f82fa6fe3654dfc7
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-04 19:57:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,3 @@ var recipes = new Object();
 function updateObjectWithKeyAndValue(recipes, key, value){
  return Object.assign( {}, object, { [key]: value })
 }
-
-var recipes = { prop: 1 }
-updateObjectWithKeyAndValue(obj, 'prop2', 2)
