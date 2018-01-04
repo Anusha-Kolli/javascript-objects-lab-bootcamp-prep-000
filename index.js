a59befd52be7304fb52da36c7bb5b5af163c18d6
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-04 19:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = new Object();
 function updateObjectWithKeyAndValue(recipes, key, value){
  return Object.assign( {}, recipes, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes,key,value){
+  recipes[key]=value;
+  return recipes;
+}
